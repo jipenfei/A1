c2156a1d17565cb3710b46effd1663c6ca6cbd43
jipenfei <921374010@qq.com>
2017-12-10 21:22:42 +0800

    haha

--- jq/jq.js
+++ jq/jq.js
@@ -5,4 +5,16 @@ $(".table-list:first li").on("mouseover",function () {
     console.log($(".table-list:first li"))
     $(".ele-home5").eq($(this).index()).addClass("ele").siblings().removeClass("ele");
 })
+console.log($(".pro-list1 ul").offset().left)
+$("#right1").on("mouseover",function () {
+    $(".pro-list1 ul").animate({
+        left:"-=1235px"
+    },"10")
+})
 
+$("#left1").on("mouseover",function () {
+
+    $(".pro-list1 ul").animate({
+        left:"+=1235px"
+    },"10")
+})
